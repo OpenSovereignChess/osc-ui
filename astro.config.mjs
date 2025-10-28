593261d6a74e1ab02e1e6eeecafeee1dd9630a5d
Al Nguyen <al.nguyen@gmail.com>
2025-10-28 19:48:06 -0400

    Add PWA support and update home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
+import AstroPWA from '@vite-pwa/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [AstroPWA()],
+});
