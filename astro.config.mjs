973f9dbac933a7d386d0405ec58888c81e24d495
Al Nguyen <al.nguyen@gmail.com>
2025-10-29 18:09:05 -0400

    Add UnoCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
-import AstroPWA from '@vite-pwa/astro';
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [AstroPWA()],
+  integrations: [
+    AstroPWA(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
 });
