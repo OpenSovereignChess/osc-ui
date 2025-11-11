74e400d3149dc48ff155460bb4cbbb726f563368
Al Nguyen <al.nguyen@gmail.com>
2025-11-10 21:14:57 -0500

    Fix board height

--- src/components/game-board/Board.tsx
+++ src/components/game-board/Board.tsx
@@ -4,19 +4,24 @@ import Square from "./Square.tsx";
 export default function Board() {
   const [el, setEl] = createSignal<HTMLElement>();
   const [size, setSize] = createSignal<number>(0);
+  const [height, setHeight] = createSignal<number>(0);
 
   createEffect(() => {
     console.log("board", el()?.clientWidth, el()?.clientHeight);
     if (el()) {
       setSize((el()?.clientWidth || 0) / 16);
+      setHeight(el()?.clientWidth || 0);
     }
   });
 
   return (
     <div
       ref={setEl}
-      class="board relative w-full h-full"
-      style="background-color: cyan"
+      class="board relative w-full"
+      style={{
+        "background-color": "cyan",
+        "height": height() + "px",
+      }}
     >
       <For each={Array(256)}>
         {(_, index) => <Square index={index()} size={size()} />}
