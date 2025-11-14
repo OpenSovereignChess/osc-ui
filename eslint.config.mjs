30770d55dcbe171e91c9d0eda92960b55019c7af
Al Nguyen <al.nguyen@gmail.com>
2025-11-14 07:18:29 -0500

    Add typescript eslint rules

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import astroEslint from "eslint-plugin-astro";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  ...astroEslint.configs.recommended,
+);
