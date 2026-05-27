25df8df3888c5d23d7d7140d1b0b73e899a12fda
Al Nguyen <al.nguyen@gmail.com>
2026-05-26 21:28:05 -0400

    Update dependencies

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { getViteConfig } from "astro/config";
+import type { ViteUserConfig } from "vitest/config";
 
 export default getViteConfig({
   test: {},
-});
+} as Parameters<typeof getViteConfig>[0] & ViteUserConfig);
